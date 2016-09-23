73229c12fc46372301e1b377ee05564d547ace13
Nathan Ridley <axefrog@gmail.com>
2016-09-23 23:24:13 +1000

    Fix error when switching between files

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function deactivate() {
 
 class LineTrimmer {
     private _disposable: vscode.Disposable;
-    private _lines = new Set<number>();
+    private _lines = new WeakMap<any, Set<number>>()
 
     constructor() {
         this._disposable = vscode.window.onDidChangeTextEditorSelection(this.onChangeSelection, this);
@@ -19,18 +19,21 @@ class LineTrimmer {
         const editor = vscode.window.activeTextEditor;
         const doc = editor.document;
         const lines = new Set<number>(e.selections.map(sel => sel.active.line));
-        this._lines.forEach(lineNum => {
-            if(!lines.has(lineNum)) {
-                const line = doc.lineAt(lineNum);
-                if(!line) return;
-                const text = line.isEmptyOrWhitespace ? '' :
-                    line.text.replace(/[ \t]+$/, '');
-                if(line.text !== text) {
-                    editor.edit(ed => ed.replace(line.range, text));
+        const previousLines = this._lines.get(doc);
+        if(previousLines) {
+            previousLines.forEach(lineNum => {
+                if(!lines.has(lineNum)) {
+                    const line = doc.lineAt(lineNum);
+                    if(!line) return;
+                    const text = line.isEmptyOrWhitespace ? '' :
+                        line.text.replace(/[ \t]+$/, '');
+                    if(line.text !== text) {
+                        editor.edit(ed => ed.replace(line.range, text));
+                    }
                 }
-            }
-        });
-        this._lines = lines;
+            });
+        }
+        this._lines.set(doc, lines);
     }
 
     dispose() {
